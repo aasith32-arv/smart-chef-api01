8c92e6be501a36d558189237696a7f45efeca2f1
aasith32-arv <aasith32@gmail.com.com>
2026-08-07 15:19:12 +0530

    Add __init__ support

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models.favorite import Favorite
+from app.models.ingredient import Ingredient
+from app.models.recipe import Recipe
+from app.models.token_blocklist import TokenBlocklist
+from app.models.user import User
+
+__all__ = ["User", "Recipe", "Ingredient", "Favorite", "TokenBlocklist"]
+
