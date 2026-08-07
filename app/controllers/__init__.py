5edbc61806378aaa0c2af5a84934824a5a7dc9d5
aasith32-arv <aasith32@gmail.com.com>
2026-08-07 15:19:12 +0530

    Add __init__ support

---
+++ app/controllers/__init__.py
@@ -0,0 +1,15 @@
+from app.controllers.ai_controller import AIController
+from app.controllers.auth_controller import AuthController
+from app.controllers.calculator_controller import CalculatorController
+from app.controllers.favorite_controller import FavoriteController
+from app.controllers.recipe_controller import RecipeController
+from app.controllers.recommendation_controller import RecommendationController
+
+__all__ = [
+    "AIController",
+    "AuthController",
+    "RecipeController",
+    "CalculatorController",
+    "RecommendationController",
+    "FavoriteController",
+]
